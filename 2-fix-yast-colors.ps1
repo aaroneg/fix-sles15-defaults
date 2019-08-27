5111ffb44e3c173a086d8a8d0e8bdc5cf7393854
Aaron <aaroneg@users.noreply.github.com>
2019-08-27 14:48:35 -0500

    Update 2-fix-yast-colors.ps1
    better default theme

--- 2-fix-yast-colors.ps1
+++ 2-fix-yast-colors.ps1
@@ -1,4 +1,4 @@
 #! /usr/bin/pwsh
 copy-item /etc/sysconfig/yast2 ~/yast2.bak
 $regex='Y2NCURSES_COLOR_THEME=""'
-(get-content /etc/sysconfig/yast2) -replace $regex, 'Y2NCURSES_COLOR_THEME="mono"'|Set-content /etc/sysconfig/yast2
+(get-content /etc/sysconfig/yast2) -replace $regex, 'Y2NCURSES_COLOR_THEME="inverted"'|Set-content /etc/sysconfig/yast2
